5605a8556936b4ee2b26cb067762e690b3051d84
Tahim Kader <tahimkader@Tahims-MacBook-Pro.local>
2017-05-02 19:42:51 -0700

    updates

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
-
+from django.utils import timezone
 from ..add_item.models import Product, Tag
-import datetime
+from datetime import datetime
+
 
 
 # Create your views here.(
 def index(request):
     # thirtyday=[]
-    # date_format = "%m/%d/%Y"
     # product=Product.objects.all()
+    # today = datetime.now()
+    # today.replace(tzinfo=timezone.utc)
     # for products in product:
-    #     delta = (date_format.strptime(products.created_at, date_format)) - (date_format.strptime(datetime.datetime.now(), date_format))
+    #     productdt=products.created_at
+    #     productdt.replace(tzinfo=timezone.utc)
+    #     delta = today-productdt
     #     if delta.days <= 30:
     #         thirtyday.append(products)
 
